e72897d6c2a06d3e16add4d3f7a996592da28675
Jake Correa <jcorrea257@gmail.com>
2015-12-16 22:22:16 -0500

    Add ios and android platforms. Bring in ngCordova dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@ require('../bower_components/ionic/js/ionic.bundle');
 require('../bower_components/angular-visjs/angular-vis');
 require('../bower_components/ion-alpha-scroll/src/ion-alpha-scroll');
 require('../bower_components/ionic-filter-bar/dist/ionic.filter.bar');
+require('../bower_components/ngCordova/dist/ng-cordova');
 
 window.Parse = require('../bower_components/parse/parse');
 window.vis = require('../bower_components/vis/dist/vis');
@@ -11,7 +12,7 @@ require('./services');
 
 module.exports = angular.module('LinedUp', [
   'ionic',
-  // 'ngCordova',
+  'ngCordova',
   'ui.router',
 
   'ngVis',
